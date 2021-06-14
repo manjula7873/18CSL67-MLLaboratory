f1330b31274b9a57c86e08d9a6c61f1100e0f405
manjula7873 <72287964+manjula7873@users.noreply.github.com>
2021-06-14 16:57:40 +0530

    Add files via upload
    LAB TERMWORKS

---
+++ AI&ML TW2.py
@@ -0,0 +1,44 @@
+[16:16, 14/06/2021] m@njul@°°💙: from collections import defaultdict
+
+class Graph:
+
+    def _init_(self):
+        self.graph=defaultdict(list)
+
+    def addEdge(self,vertex,neighbours):
+        self.graph[vertex].extend(neighbours)
+
+    def bestFirstSearch(self,goal,distance):
+        queue=[]
+        state='A'
+        path=['A']
+        while True:
+            if state==goal:
+                print("SUCCESS: Path is",','.join(path))
+                return True
+            
+            queue=self.graph[state]+queue
+            queue.sort(key=lambda x:distance[ord(x)-65])
+
+            if queue==[]:
+                return False
+            state=queue.pop(0)
+            path.append(state)
+            
+def main():
+    numOfNodes=int(input("Enter the number of nodes in graph:"))
+    g=Graph()
+
+    distance=[]
+    goal=input("Enter the goal node:")
+    
+    for i in range(numOfNodes):
+        successors=input("Enter the successors of node %s:"%chr(65+i)).split()
+        distance.append(int(input("Enter the straight line distance from node %s to the goal node %s:"%(chr(65+i),goal))))
+        g.addEdge(chr(65+i),successors)
+
+    if not g.bestFirstSearch(goal,distance):
+        print("FAILURE!!!")
+
+if _name=="main_":
+    main()
